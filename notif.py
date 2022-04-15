4e75f5f8d201f37d8701bf5df3b1248734900959
Otto <51783206+HEPOSHEIKKI@users.noreply.github.com>
2022-04-15 13:18:16 +0300

    Fixed notification string length

--- notif.py
+++ notif.py
@@ -11,7 +11,10 @@ def notify(string):
     root.attributes("-topmost", True)
     root.resizable(0,0)
     text = LabelFrame(root, text="Now playing:", fg="white", bg="gold", width="250", height="75",bd=2).place(x=0, y=0)
-    song = LabelFrame(root, text=string[0:22] + "...", fg="white",bg="gold",font=("consolas", 12,"bold"), width="230", height="30", bd=0).place(x=10, y=35)
+    if len(string) >= 22:
+        song = LabelFrame(root, text=string[0:22] + "...", fg="white",bg="gold",font=("consolas", 12,"bold"), width="230", height="30", bd=0).place(x=10, y=35)
+    else:
+        song = LabelFrame(root, text=string, fg="white",bg="gold",font=("consolas", 12,"bold"), width="230", height="30", bd=0).place(x=10, y=35)
 
     root.update()
     root.after(5000, root.destroy)
